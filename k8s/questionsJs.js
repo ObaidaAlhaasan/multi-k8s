7c40bdd1203ed2756516844f4d510985bbb937b0
Obaida Alhassan <obaida@terkwaz.com>
2020-07-04 23:04:39 +0300

    first commit

---
+++ k8s/questionsJs.js
@@ -0,0 +1,13 @@
+const questions = ["D", "C", "C", "A", "D", "A", "C", "A", "B", "A", "C", "D", "C", "A", "C", "A", "D", "C", "C" , "C", "D" , "A", "B", "B" , "C", "C" , "A" , "C" , "A" , "A?D", "B", "C" , "A", "D", "B", "B", "A" , "C", "C", "A", "C", "D", "A", "A", "C", "B", "B", "B", "B", "A", "C", "A", "C", "B", "A", "A", "B", "B", "A", "C", "B", "A", "A", "D", "A", "D", "B", "A", "A", "B", "A", "B", "B", "A", "B", "B", "D", "A", "C", "A", "C", "B", "B", "B", "C", "C", "A", "B", "B", "A", "C", "D", "D", "B", "A", "B", "C" , "A", "D", "C", "C", "C", "B", "C", "C", "B", "D", "A", "D", "B", "D", "A", "D", "A" , "C", "B", "B", "D"  , "B", "B", "B", "C", "B", "A", "C", "D", "B" , "B", "A", "B", "C", "B", "D", "D", "B", "C", "C", "B", "A" , "D", "A", "C", "B" ,"D" , "B", "C", "D", "A", "A", "C", "A", "B", "A", "C", "C", "B"];
+
+
+
+const answers = [true,true, ];
+
+
+
+const reasons = [];
+
+
+
+console.log(questions.length);
